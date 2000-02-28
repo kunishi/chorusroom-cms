71aac1e98e4f1726e84216af973ca9ac2149cd3b
kunishi <devnull@localhost>
2000-02-28 12:41:05 +0000

    added Kaffe and Linux JDK support.

--- src/config.mk
+++ src/config.mk
@@ -1,13 +1,34 @@
 # Common macro definitions.
-# $Id: config.mk,v 1.21 1999/12/18 14:28:47 kunishi Exp $
+# $Id: config.mk,v 1.22 2000/02/28 12:41:05 kunishi Exp $
 #
 
-JDK_LIBDIR=	/usr/local/jdk1.1.8/lib
-JDK_CLASSPATH=	${JDK_LIBDIR}/classes.zip:${JDK_LIBDIR}
-JAVA_CLASSES_DIR=	/usr/local/share/java/classes
+LOCALBASE=	/usr/local
+ifdef USE_LINUX_JDK
+JDK_TOPDIR=	${LOCALBASE}/linux-jdk1.2
+else
+JDK_TOPDIR=	${LOCALBASE}/jdk1.1.8
+endif
+JDK_LIBDIR=	${JDK_TOPDIR}/lib
+
+JAVA_CLASSES_DIR=	${LOCALBASE}/share/java/classes
 XT_CLASSPATH=	${JAVA_CLASSES_DIR}/xt.jar:${JAVA_CLASSES_DIR}/sax.jar
 XML4J_CLASSPATH= ${JAVA_CLASSES_DIR}/xml4j.jar:${JAVA_CLASSES_DIR}/xml4jSamples.jar
 LOTUSXSL_CLASSPATH=	${JAVA_CLASSES_DIR}/lotusxsl.jar:${JAVA_CLASSES_DIR}/lotusxslbsf.jar:${JAVA_CLASSES_DIR}/js.jar
+
+ifdef USE_KAFFE
+JAVA=		${LOCALBASE}/bin/kaffe
+JDK_CLASSPATH=	${LOCALBASE}/share/kaffe/kjc.jar:${LOCALBASE}/share/kaffe/Klasses.jar
+else
+JAVA=		${JDK_TOPDIR}/bin/java
+XSLT_ENV+=	JAVA_COMPILER=${JAVA_COMPILER}
+ifdef USE_LINUX_JDK
+JAVA_COMPILER?=	
+JDK_CLASSPATH=	${JDK_LIBDIR}/tools.jar:${JDK_LIBDIR}/dt.jar
+else
+JAVA_COMPILER?=	tya
+JDK_CLASSPATH=	${JDK_LIBDIR}/classes.zip:${JDK_LIBDIR}
+endif
+endif
 
 ifdef USE_LOTUSXSL
 XSLT_CLASSPATH=	${LOTUSXSL_CLASSPATH}
@@ -20,9 +41,7 @@ XSLT_CLASS=	com.jclark.xsl.sax.Driver
 endif
 
 CLASSPATH=	${XSLT_CLASSPATH}:${XML4J_CLASSPATH}:${JDK_CLASSPATH}
-XSLT_PROC=	java -classpath ${CLASSPATH} ${XSLT_OPTS} ${XSLT_CLASS}
-JAVA_COMPILER?=	tya
-XSLT_ENV+=	JAVA_COMPILER=${JAVA_COMPILER}
+XSLT_PROC=	${JAVA} -classpath ${CLASSPATH} ${XSLT_OPTS} ${XSLT_CLASS}
 
 UTF2ASCII=	hutrans
 ASCII2EUC=	tcs -f utf -t ujis
@@ -31,10 +50,10 @@ HTML_FORMAT=	tidy -iso2022
 SYNC_TOOL=	rsync -au
 
 CP=		cp
-ENV=		env
+ENV?=		env
 
 RELPATH!=	.
-INSTTOPDIR=	/usr/local/www/data/chorusRoom/
+INSTTOPDIR=	${LOCALBASE}/www/data/chorusRoom/
 
 XHTML10_XSL=	${SRCTOPDIR}/xhtml10.xsl
 XHTML10_XSL_UTF8=	${SRCTOPDIR}/xhtml10-utf8.xsl
