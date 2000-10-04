a783b9c390104f56f00effd518b49a3ff482e250
kunishi <devnull@localhost>
2000-10-04 05:13:39 +0000

    new file.

---
+++ lib/scripts/fix-xmldecl-duplication.rb
@@ -0,0 +1,21 @@
+#!/usr/local/bin/ruby
+#
+# a script for fixing irregular result of Xalan
+# $Id: fix-xmldecl-duplication.rb,v 1.1 2000/10/04 05:13:39 kunishi Exp $
+
+$xmldecl = 'no'
+
+ARGV.each {
+  |file|
+  $fd = File.open(file)
+  $newfd = File.new(file + '.new', "w+")
+  while $_ = $fd.gets
+    if $_ =~ /^<\?xml version="1.0" / then
+      if $xmldecl == 'yes' then next end
+      $xmldecl = 'yes'
+    end
+    $newfd.print $_
+  end
+  $fd.close
+  $newfd.close
+}
