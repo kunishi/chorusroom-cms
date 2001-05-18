449e95809d408bce50d0172c72d555e46566e61e
kunishi <devnull@localhost>
2001-05-18 11:52:49 +0000

    added: Arcadelt Consort, Kobe Womens' Univ. Chorus Club OG, Handa Mixed Choir. changed URL: Nakatsu Yukinone Choir, Atsugi Mixed Choir.

--- src/scripts/xalan.sh
+++ src/scripts/xalan.sh
@@ -4,7 +4,7 @@ JAVA_HOME=/usr/local/jdk1.2.2; export JAVA_HOME
 JAVA=${JAVA_HOME}/bin/java
 
 XERCES_CLASSPATH=/usr/local/share/java/classes/xerces.jar
-XALAN_CLASSPATH=/usr/local/share/java/classes/xalan/xalan.jar:/usr/local/share/java/classes/xalan/bsf.jar
+XALAN_CLASSPATH=/usr/local/xalan-j2.0.1/bin/xalan.jar:/usr/local/xalan-j2.0.1/bin/bsf.jar
 XALAN_CLASS=org.apache.xalan.xslt.Process
 
 ${JAVA} -classpath ${XERCES_CLASSPATH}:${XALAN_CLASSPATH} ${XALAN_CLASS} $*
