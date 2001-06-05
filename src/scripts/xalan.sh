ca772a93ba09e3b9d5bf6ba458577732b5f418d3
kunishi <devnull@localhost>
2001-06-05 11:03:12 +0000

    use javavm rather than java itself.

--- src/scripts/xalan.sh
+++ src/scripts/xalan.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-JAVA_HOME=/usr/local/jdk1.2.2; export JAVA_HOME
-JAVA=${JAVA_HOME}/bin/java
+if [ -f /usr/local/bin/javavm ]; then
+	JAVA=/usr/local/bin/javavm
+else
+	JAVA_HOME=/usr/local/jdk1.2.2; export JAVA_HOME
+	JAVA=${JAVA_HOME}/bin/java
+fi
 
 XERCES_CLASSPATH=/usr/local/share/java/classes/xerces.jar
 XALAN_CLASSPATH=/usr/local/xalan-j2.1.0/bin/xalan.jar:/usr/local/xalan-j2.1.0/bin/bsf.jar
