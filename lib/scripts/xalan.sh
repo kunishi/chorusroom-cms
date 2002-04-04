da4289f393abf1ebe1f5f893c3534212d592a97b
kunishi <devnull@localhost>
2002-04-04 15:16:27 +0000

    Oops, CLASSPATH was not fixed.

--- lib/scripts/xalan.sh
+++ lib/scripts/xalan.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 XALAN_HOME=/usr/local/share/java/xalan-j-2.3.1
-CLASSPATH=${XALAN_HOME}/bin/xerces.jar:${XALAN_HOME}/bin/xalan.jar:${XALAN_HOME}/bin/bsf.jar
+CLASSPATH=${XALAN_HOME}/bin/xalan.jar:${XALAN_HOME}/bin/xml-apis.jar:${XALAN_HOME}/bin/xercesImpl.jar
 export CLASSPATH
 XALAN_CLASS=org.apache.xalan.xslt.Process
 
