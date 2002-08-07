46354928e81798fbb8cf72cc7ec1667bb33d51c1
kunishi <devnull@localhost>
2002-08-07 16:53:22 +0000

    obsolete html.utf8.  UTF-8 files' extensions are changed to .xhtml.

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BUILD_DIR=./build
-BUILD_ISO_DIR=${BUILD_DIR}/html.iso/
+BUILD_ISO_DIR=${BUILD_DIR}/html/
 BUILD_UTF8_DIR=${BUILD_DIR}/html.utf8/
 
 WWW_HOST=www.chorusroom.org
@@ -10,4 +10,4 @@ WWW_ISO_DIR=${WWW_DIR}/chorusRoom/
 WWW_UTF8_DIR=${WWW_DIR}/chorusRoom.utf8/
 
 rsync -avuIz -e ssh ${BUILD_ISO_DIR} ${WWW_HOST}:${WWW_ISO_DIR}
-rsync -avuIz -e ssh ${BUILD_UTF8_DIR} ${WWW_HOST}:${WWW_UTF8_DIR}
+#rsync -avuIz -e ssh ${BUILD_UTF8_DIR} ${WWW_HOST}:${WWW_UTF8_DIR}
