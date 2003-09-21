ab6d0a04f35b77da9fff4f360a4098888eb84173
kunishi <devnull@localhost>
2003-09-21 05:10:46 +0000

    add 56th JCA Hyogo.

--- prog/java/build.sh
+++ prog/java/build.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-RELAXERLIBDIR=/usr/local/lib/relaxer
-ANTLIBDIR=/usr/local/share/java/apache-ant/lib
+
+if [ `uname -o` = 'Cygwin' ]; then
+   DRIVE="c:"
+   JAVAC=$JAVA_HOME/bin/javac
+else
+   JAVAC=javac
+fi
+
+RELAXERLIBDIR=$DRIVE/usr/local/lib/relaxer
+ANTLIBDIR=$DRIVE/usr/local/share/java/apache-ant/lib
 
 RELAXERCP=${RELAXERLIBDIR}/Relaxer.jar:${RELAXERLIBDIR}/RelaxerOrg.jar:${RELAXERLIBDIR}/isorelax.jar:${RELAXERLIBDIR}/relaxngDatatype.jar:${RELAXERLIBDIR}/xerces.jar:${RELAXERLIBDIR}/xsdlib.jar
 ANTCP=${ANTLIBDIR}/ant.jar
 
-javac -classpath .:${RELAXERCP}:${ANTCP} org/chorusroom/ant/*.java
+$JAVAC -classpath .:${RELAXERCP}:${ANTCP} org/chorusroom/ant/*.java
