001c7e0e27ed824729cc2f89ff487e45c2a3f4dc
kunishi <devnull@localhost>
2004-03-25 14:29:46 +0000

    normalization to use another table.

--- lib/sql/mysql-init-link-db.sql
+++ lib/sql/mysql-init-link-db.sql
@@ -5,15 +5,16 @@ CREATE TABLE link (
        name VARCHAR(30) NOT NULL,
        yomi VARCHAR(70) NOT NULL,
        url VARCHAR(100) NOT NULL,
-       keyword1 VARCHAR(15) NOT NULL,
-       keyword2 VARCHAR(15),
-       keyword3 VARCHAR(15),
-       keyword4 VARCHAR(15),
-       keyword5 VARCHAR(15),
        comment TEXT NULL,
        created DATETIME NOT NULL,
        modified TIMESTAMP,
        PRIMARY KEY (id));
 
+CREATE TABLE link_key (
+       id MEDIUMINT NOT NULL,
+       keyword VARCHAR(15));
+
 GRANT ALL PRIVILEGES ON chorusroom.link TO 'chorusroom'@'localhost'
+      IDENTIFIED BY 'pizzetti';
+GRANT ALL PRIVILEGES ON chorusroom.link_key TO 'chorusroom'@'localhost'
       IDENTIFIED BY 'pizzetti';
