f4b17aa4f4bac4399c525f1738da7d520d9e76d8
www <devnull@localhost>
2004-05-19 15:56:34 +0000

    concours result Importer.

---
+++ db/concours-v20/DWimporter.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'xml/xslt'
+require 'rexml/document'
+
+xslt = XML::XSLT.new()
+xslt.xmlfile = ARGV[0]
+xslt.xslfile = 'concours2body.xsl'
+out = xslt.serve()
+xslt.xslfile = 'concours2title.xsl'
+title = xslt.serve()
+
+doc = REXML::Document.new()
+doc << REXML::XMLDecl.new('1.0', 'utf-8')
+el = doc.add_element "templateItems",
+  {
+  "codeOutsiteHTMLIsLocked" => "false", 
+  "template" => "/Templates/default.dwt"
+  }
+e = el.add_element "item", { "name" => "head" }
+e = el.add_element "item", { "name" => "doctitle" }
+e.add_text REXML::CData.new(title)
+e = el.add_element "item", { "name" => "DocumentBody" }
+e.add_text REXML::CData.new(out)
+
+print doc.to_s
